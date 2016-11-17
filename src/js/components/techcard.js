eef009347c7201725915588f5bb8194f7420603f
Xesenix <pkapalla@xesenix.pl>
2016-11-17 08:11:33 +0100

    fix

--- src/js/components/techcard.js
+++ src/js/components/techcard.js
@@ -6,6 +6,8 @@ function Techcard(game, x, y) {
 	GameObject.call(this, game, 0, 0, 0, 'techcard', 'techcard');
 	this.x = x;
 	this.y = y;
+	
+	this.body.setSize(24, 24, 24, 24);
 }
 
 Techcard.prototype = Object.create(GameObject.prototype);
